774455c70b71258ff00fa3ea5063b36e7b1059c4
hrxgaurav <magicgalaxy0@gmail.com>
2022-11-26 12:10:39 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/App.css';
 
 const Loader = () => <div id="loader">Loading...</div>
 
 
 const App = () => {
+  const [data,setData] = useState([])
+  const [type,setType] = useState("education");
+  const [load, setLoad] = useState("")
 
   
+  const getData = async () => {
+    fetch(
+      `http://www.boredapi.com/api/activity?type=${type}`
+    )
+      .then((results) => results.json())
+      .then((datAagain) => setData(datAagain)); 
+  };
+
+
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function education() {
+    setLoad(data)
+    setType("education")
+    getData();
+  }
+  
+  function recreational() {
+    setLoad(data)
+    setType("recreational")
+    getData();
+  }
+
 
   return (
     <div id="main">
+    <div id="activity">{data==load ? <Loader/> : data.activity}</div>
+
+
+    <button id="btn-education" onClick={education}>Education</button>
+    <button id="btn-recreation" onClick={recreational}>Recreational</button>
     
     </div>
   )
