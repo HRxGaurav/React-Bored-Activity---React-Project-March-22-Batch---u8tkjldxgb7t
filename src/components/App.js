0a5f81f1160cebe974121c3afb9d81cf1dabadb4
hrxgaurav <magicgalaxy0@gmail.com>
2022-11-28 09:09:45 +0530

    optimise

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 
-const Loader = () =><div id="loader">Loading...</div> 
-
-const LoadingStatus = {
-  NOT_STARTED: "NOT_STARTED",
-  IN_PROGRESS: "IN_PROGRESS",
-  SUCCESS: "SUCCESS",
-};
-
+const Loader = () => <div id="loader">Loading...</div>;
 
 const App = () => {
-  const [data,setData] = useState([])
-  const [type,setType] = useState("education");
-  const [isLoading, setIsLoading] = React.useState(LoadingStatus.IN_PROGRESS);
+  const [data, setData] = useState([]);
+  const [type, setType] = useState("education");
+  const [isLoading, setIsLoading] = useState(false);
 
-  
   const getData = async () => {
-    // fetch(
-    //   `http://www.boredapi.com/api/activity?type=${type}`
-    // )
+    setIsLoading(false);
 
-    // .then((results) => results.json(), console.log(isLoading))
-    // .then((datAagain) => setData(datAagain), setIsLoading(LoadingStatus.SUCCESS), console.log(isLoading)); 
-
-    const response = await fetch(`http://www.boredapi.com/api/activity?type=${type}`);
+    const response = await fetch(
+      `http://www.boredapi.com/api/activity?type=${type}`
+    );
     const json = await response.json();
     setData(json);
-    setIsLoading(LoadingStatus.SUCCESS)
-
+    setIsLoading(true);
   };
 
-
   useEffect(() => {
-    setIsLoading(LoadingStatus.IN_PROGRESS)
     getData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function education() {
-    setIsLoading(LoadingStatus.IN_PROGRESS)
-    setType("education")
+    setType("education");
     getData();
-      }
+  }
 
   function recreational() {
-    setIsLoading(LoadingStatus.IN_PROGRESS)
-    setType("recreational")
+    setType("recreational");
     getData();
   }
 
   return (
     <div id="main">
-    { isLoading=="IN_PROGRESS" && <Loader/> }
-    {isLoading=="SUCCESS" && <div id="activity">{data.activity}</div>}
+      {!isLoading && <Loader />}
+      {isLoading && <div id="activity">{data.activity}</div>}
 
-
-    <button id="btn-education" onClick={education}>Education</button>
-    <button id="btn-recreation" onClick={recreational}>Recreational</button>
-    
+      <button id="btn-education" onClick={education}>
+        Education
+      </button>
+      <button id="btn-recreation" onClick={recreational}>
+        Recreational
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
